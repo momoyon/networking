7bc084fc7baa1e19e3455c878cd30793e6d329f0
momoyon <momoyon@momoyon.org>
2025-05-30 08:14:45 +0500

    [engine.h] Add rect functions.

--- include/engine.h
+++ include/engine.h
@@ -7,9 +7,14 @@
 #include <raylib.h>
 #include <raymath.h>
 
+#define COMMONLIB_REMOVE_PREFIX
+#include <commonlib.h>
+
 // Vector helpers
 Vector2 v2xx(float v);
 Vector2 v2(float x, float y);
+#define v2_sub Vector2Subtract
+#define v2_mag2 Vector2LengthSqr
 
 // Vector2i
 typedef struct {
@@ -18,6 +23,14 @@ typedef struct {
 
 Vector2i v2vi(Vector2 v);
 bool v2i_equal(Vector2i a, Vector2i b);
+
+// Rectangle
+bool rect_contains_point(Rectangle r1, Vector2 p);
+bool rect_contains_rect(Rectangle r1, Rectangle r2);
+bool rect_intersects_rect(Rectangle r1, Rectangle r2);
+// bool rect_resolve_rect_collision(Rectangle* rect1, const Rectangle rect2);
+// void rect_get_3d_points(Rectangle rect, Vector3f* p0, Vector3f* p1, Vector3f* p2, Vector3f* p3);
+// void rect_get_points(Rectangle rect, Vector2* p0, Vector2* p1, Vector2* p2, Vector2* p3);
 
 // Window
 RenderTexture2D init_window(int screen_width, int screen_height, float scl, const char *title, int *width_out, int *height_out);
@@ -62,6 +75,99 @@ Vector2i v2vi(Vector2 v) { return CLITERAL(Vector2i) { (int)v.x, (int)v.y }; }
 bool v2i_equal(Vector2i a, Vector2i b) {
 	return a.x == b.x && a.y == b.y;
 }
+
+// Rectangle
+bool rect_contains_point(Rectangle r1, Vector2 p) {
+  return (p.x >= r1.x && p.x < r1.x + r1.width &&
+	  p.y >= r1.y && p.y < r1.y + r1.height);
+}
+
+bool rect_contains_rect(Rectangle r1, Rectangle r2) {
+  return (rect_contains_point(r1, v2(r2.x, r2.y)) &&
+	  rect_contains_point(r1, (Vector2) {r2.x + r2.width,
+					     r2.y + r2.height}));
+}
+
+bool rect_intersects_rect(Rectangle r1, Rectangle r2) {
+  const float rect1_l = r1.x;
+  const float rect1_r = r1.x+r1.width;
+  const float rect1_t = r1.y;
+  const float rect1_b = r1.y+r1.height;
+
+  const float rect2_l = r2.x;
+  const float rect2_r = r2.x+r2.width;
+  const float rect2_t = r2.y;
+  const float rect2_b = r2.y+r2.height;
+
+  return (rect1_r >= rect2_l &&
+	  rect1_l <= rect2_r &&
+	  rect1_t <= rect2_b &&
+	  rect1_b >= rect2_t);
+}
+
+bool rect_resolve_rect_collision(Rectangle* rect1, const Rectangle rect2) {
+  const float rect1_l = rect1->x;
+  const float rect1_r = rect1->x+rect1->width;
+  const float rect1_t = rect1->y;
+  const float rect1_b = rect1->y+rect1->height;
+
+  const float rect2_l = rect2.x;
+  const float rect2_r = rect2.x+rect2.width;
+  const float rect2_t = rect2.y;
+  const float rect2_b = rect2.y+rect2.height;
+
+  // resolve collision only if it ever happens
+  if (rect_intersects_rect(*rect1, rect2)) {
+    Vector2 cb2_bot = {0.f, rect2_b};
+    Vector2 cb1_top = {0.f, rect1_t};
+    float d2_top = v2_mag2(v2_sub(cb1_top, cb2_bot));
+    Vector2 cb2_left = {rect2_l, 0.f};
+    Vector2 cb1_right = {rect1_r, 0.f};
+    float d2_right = v2_mag2(v2_sub(cb1_right, cb2_left));
+    Vector2 cb2_right = {rect2_r, 0.f};
+    Vector2 cb1_left = {rect1_l, 0.f};
+    float d2_left = v2_mag2(v2_sub(cb1_left, cb2_right));
+    Vector2 cb2_top = {0.f, rect2_t};
+    Vector2 cb1_bot = {0.f, rect1_b};
+    float d2_bot = v2_mag2(v2_sub(cb1_bot, cb2_top));
+
+    float min_d2 = fminf(d2_top, fminf(d2_left, fminf(d2_right, d2_bot)));
+
+    if (min_d2 == d2_top) {
+      rect1->y = rect2_b;
+    } else if (min_d2 == d2_left) {
+      rect1->x = rect2_r;
+    } else if (min_d2 == d2_right) {
+      rect1->x = rect2_l - rect1->width;
+    } else if (min_d2 == d2_bot) {
+      rect1->y = rect2_t - rect1->height;
+    } else {
+      ASSERT(0, "UNREACHABLE");
+    }
+    return true;
+  }
+  return false;
+}
+
+// void rect_get_3d_points(Rectangle rect, Vector3f* p0, Vector3f* p1, Vector3f* p2, Vector3f* p3) {
+//   Vector2 p0_ = v2_add(rect.pos, (Vector2) {0.f, 0.f});
+//   Vector2 p1_ = v2_add(rect.pos, (Vector2) {rect.size.x, 0.f});
+//   Vector2 p2_ = v2_add(rect.pos, (Vector2) {rect.size.x, rect.size.y});
+//   Vector2 p3_ = v2_add(rect.pos, (Vector2) {0.f, rect.size.y});
+//
+//   *p0 = (Vector3f) {p0_.x, p0_.y, 0.f};
+//   *p1 = (Vector3f) {p1_.x, p1_.y, 0.f};
+//   *p2 = (Vector3f) {p2_.x, p2_.y, 0.f};
+//   *p3 = (Vector3f) {p3_.x, p3_.y, 0.f};
+// }
+//
+// void rect_get_points(Rectangle rect, Vector2* p0, Vector2* p1, Vector2* p2, Vector2* p3) {
+//   *p0 = v2_add(rect.pos, (Vector2) {0.f, 0.f});
+//   *p1 = v2_add(rect.pos, (Vector2) {rect.size.x, 0.f});
+//   *p2 = v2_add(rect.pos, (Vector2) {rect.size.x, rect.size.y});
+//   *p3 = v2_add(rect.pos, (Vector2) {0.f, rect.size.y});
+// }
+//
 
 // Setup
 RenderTexture2D init_window(int screen_width, int screen_height, float scl, const char *title, int *width_out, int *height_out) {
