a0386084255dde4674c0e2f0c35bf21503af9102
ictadmin <ictadm@jawakara.com>
2025-07-15 17:36:34 +0600

    [test_ipv4_class.c] can check ranges

--- test_ipv4_class.c
+++ test_ipv4_class.c
@@ -3,7 +3,6 @@
 #define COMMONLIB_IMPLEMENTATION
 #include <commonlib.h>
 
-
 // Crash on failure
 int str_to_int(const char *str) {
 	char *endptr = NULL;
@@ -15,12 +14,92 @@ int str_to_int(const char *str) {
 	return converted;
 }
 
+uint8 *flip_bytes4(uint8 *bytes, uint8 *bytes_flipped) {
+	uint8 res[4] = {
+		bytes[3],
+		bytes[2],
+		bytes[1],
+		bytes[0],
+	};
+
+	bytes_flipped[0] = res[0];
+	bytes_flipped[1] = res[1];
+	bytes_flipped[2] = res[2];
+	bytes_flipped[3] = res[3];
+}
+
+uint parse_ipv4(String_view sv) {
+	sv_ltrim(&sv);
+	String_view ipv4_oct1_sv = sv_lpop_until_char(&sv, '.');
+	if (ipv4_oct1_sv.count == 0) {
+		log_error("Please pass a valid ipv4! <uint8.uint8.uint8.uint8>");
+		exit(1);
+	}
+	sv_lremove(&sv, 1); // Remove .
+	String_view ipv4_oct2_sv = sv_lpop_until_char(&sv, '.');
+	if (ipv4_oct2_sv.count == 0) {
+		log_error("Please pass a valid ipv4! <uint8.uint8.uint8.uint8>");
+		exit(1);
+	}
+	sv_lremove(&sv, 1); // Remove .
+	String_view ipv4_oct3_sv = sv_lpop_until_char(&sv, '.');
+	if (ipv4_oct3_sv.count == 0) {
+		log_error("Please pass a valid ipv4! <uint8.uint8.uint8.uint8>");
+		exit(1);
+	}
+	sv_lremove(&sv, 1); // Remove .
+	String_view ipv4_oct4_sv = sv_lpop_until_char(&sv, ' ');
+	if (ipv4_oct4_sv.count == 0) {
+		log_error("Please pass a valid ipv4! <uint8.uint8.uint8.uint8>");
+		exit(1);
+	}
+	sv_lremove(&sv, 1); // Remove SPACE
+
+	uint8 ipv4[4] = {0};
+	int   ipv4_counts[4] = { -1, -1, -1, -1 };
+	ipv4[0] = sv_to_uint(ipv4_oct1_sv, &ipv4_counts[0], 10);
+	if (ipv4_counts[0] <= 0) {
+		log_debug("Failed to convert ipv4 oct1`"SV_FMT"` to int!", SV_ARG(ipv4_oct1_sv));
+		exit(1);
+	}
+	ipv4[1] = sv_to_uint(ipv4_oct2_sv, &ipv4_counts[1], 10);
+	if (ipv4_counts[1] <= 0) {
+		log_debug("Failed to convert ipv4 oct2`"SV_FMT"` to int!", SV_ARG(ipv4_oct2_sv));
+		exit(1);
+	}
+	ipv4[2] = sv_to_uint(ipv4_oct3_sv, &ipv4_counts[2], 10);
+	if (ipv4_counts[2] <= 0) {
+		log_debug("Failed to convert ipv4 oct3`"SV_FMT"` to int!", SV_ARG(ipv4_oct3_sv));
+		exit(1);
+	}
+	ipv4[3] = sv_to_uint(ipv4_oct4_sv, &ipv4_counts[3], 10);
+	if (ipv4_counts[3] <= 0) {
+		log_debug("Failed to convert ipv4 oct4`"SV_FMT"` to int!", SV_ARG(ipv4_oct4_sv));
+		exit(1);
+	}
+
+	flip_bytes4(ipv4, ipv4);
+	uint res = *((uint *)&ipv4);
+
+	return res;
+}
+
+void usage(const char *program) {
+	log_info("Usage: %s [subcommand] [sbcmd_arg(s)]", program);
+	log_info("");
+	log_info("Subcommands: ");
+	log_info("	-max_count <max_count:int>				- Specify the maximum number of ips to check. (Increments from 0.0.0.0)");
+	log_info("	-range <min:ip> <max:ip>				- Specify the range of ips to check.");
+	log_info("	-help                    				- Prints this help message.");
+}
+
 int main(int argc, char **argv) {
-	int ipv4 = 0;
+	uint ipv4 = 0;
 
 	const char *program = shift_args(argv, argc);
 
-	int max_ipv4 = INT_MAX;
+	uint min = 0;
+	uint max = UINT_MAX;
 	if (argc > 0) {
 		const char *arg = shift_args(argv, argc);
 
@@ -37,18 +116,35 @@ int main(int argc, char **argv) {
 				return 1;
 			}
 
-			max_ipv4 = max_ipv4_converted;
+			max = max_ipv4_converted;
 		} else if (strcmp(arg, "-range") == 0) {
 			if (argc < 2) {
 				log_error("%s needs two argument!", arg);
 				return 1;
 			}
+			const char *min_str = shift_args(argv, argc);
+			const char *max_str = shift_args(argv, argc);
+			int min_ip = parse_ipv4(SV(min_str));
+			int max_ip = parse_ipv4(SV(max_str));
+
+			min = min_ip;
+			max = max_ip;
+
+			uint8 *ipv4_min = (uint8 *)&min;
+			uint8 *ipv4_max = (uint8 *)&max;
+
+		} else if (strcmp(arg, "-help") == 0) {
+			usage(program);
+			return 0;
+		} else {
+			log_error("Invalid subcommand `%s`", arg);
+			return 1;
 		}
 	}
 
-	while (ipv4 < max_ipv4) {
+	for (ipv4 = min; ipv4 <= max; ++ipv4) {
 		uint8 *ipv4__ = (uint8 *)&ipv4;
-		uint8 ipv4_[4] = {
+		uint8 ipv4_[] = {
 			ipv4__[3],
 			ipv4__[2],
 			ipv4__[1],
@@ -57,7 +153,6 @@ int main(int argc, char **argv) {
 
 		log_debug(IPV4_FMT" -> %s %s", IPV4_ARG(ipv4_), ipv4_class_as_str(determine_ipv4_class(ipv4_)), ipv4_type_as_str(determine_ipv4_type(ipv4_)));
 
-		ipv4++;
 	}
 
 	return 0;
